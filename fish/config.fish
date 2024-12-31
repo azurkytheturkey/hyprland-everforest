1a3e5e264b3a6aa2addb56ace34b749b1cfc2c5c
azurky <74217707+azurkytheturkey@users.noreply.github.com>
2024-12-31 06:41:46 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-set -x LIBVA_DRIVER_NAME radeonsi
-#set -x LIBVA_DRIVER_NAME nvidia
+#set -x LIBVA_DRIVER_NAME radeonsi
+set -x LIBVA_DRIVER_NAME nvidia
 #theme
 fish_config theme choose "Base16 Eighties"
